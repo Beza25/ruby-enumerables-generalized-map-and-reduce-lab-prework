0231fa63cefe80cb70fdf27fca5efbde8c08b675
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-24 12:44:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # Your Code Here
+#Negative 
+def map (array)
+  new = []
+  i = 0 
+  while i < array.length 
+    new.push(yield (array[i]))
+    i += 1 
+  end
+  new
+end
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    sum = array[0]
+    i = 1
+  else
+    sum = starting_point
+    i = 0
+  end
+  while i < array.length
+    sum = yield (sum, array[i])
+    i += 1
+  end
+  sum
+end
+
+  
+
+
+
+
+
